6987f3a7a06a6719e63a955a4c8ba9723ea33ca9
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 17:37:22 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,35 +1,127 @@
-void LQRPID::odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr msg) {
-    geometry_msgs::msg::PoseStamped pose_in, pose_out;
-
-    // ① 현재 odometry를 기반으로 PoseStamped 생성
-    pose_in.header = msg->header;
-    pose_in.pose = msg->pose.pose;
-
-    // ② tf2를 이용해 global_refFrame(map) 기준으로 변환
-    try {
-        tf_buffer_->transform(pose_in, pose_out, global_refFrame);  // "map"
-    } catch (tf2::TransformException &ex) {
-        RCLCPP_WARN(this->get_logger(), "Transform failed: %s", ex.what());
-        return;
-    }
-
-    // ③ 변환된 위치 좌표
-    double x = pose_out.pose.position.x;
-    double y = pose_out.pose.position.y;
-
-    // ④ 방향 추출 (tf2::Quaternion 사용)
-    tf2::Quaternion q_tf;
-    q_tf.setX(pose_out.pose.orientation.x);
-    q_tf.setY(pose_out.pose.orientation.y);
-    q_tf.setZ(pose_out.pose.orientation.z);
-    q_tf.setW(pose_out.pose.orientation.w);
-
-    double roll, pitch, yaw;
-    tf2::Matrix3x3(q_tf).getRPY(roll, pitch, yaw);  // ⚠️ tf2의 Quaternion을 사용해야 함
-
-    // ⑤ 속도 정보
-    double velocity = msg->twist.twist.linear.x;
-
-    // ⑥ 메시지 전송
-    publish_message(x, y, yaw, velocity);
-}
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 374422204
+    frame_id: ego_racecar/front_right_hinge
+  child_frame_id: ego_racecar/front_right_wheel
+  transform:
+    translation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 374422204
+    frame_id: map
+  child_frame_id: laser
+  transform:
+    translation:
+      x: -11.070623243221863
+      y: -0.02955028754656039
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: -0.0
+      z: -0.0003194320854121876
+      w: -0.9999999489815701
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 400523943
+    frame_id: sim
+  child_frame_id: ego_racecar/base_link
+  transform:
+    translation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 400523943
+    frame_id: ego_racecar/base_link
+  child_frame_id: ego_racecar/laser
+  transform:
+    translation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 400523943
+    frame_id: ego_racecar/front_left_hinge
+  child_frame_id: ego_racecar/front_left_wheel
+  transform:
+    translation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 400523943
+    frame_id: ego_racecar/front_right_hinge
+  child_frame_id: ego_racecar/front_right_wheel
+  transform:
+    translation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+---
+transforms:
+- header:
+    stamp:
+      sec: 1750062950
+      nanosec: 400523943
+    frame_id: map
+  child_frame_id: laser
+  transform:
+    translation:
+      x: -11.035759456914924
+      y: -0.030613278861875502
+      z: 0.0
+    rotation:
+      x: 0.0
+      y: -0.0
+      z: -0.00042778460234708064
+      w: -0.9999999085001628
+---
