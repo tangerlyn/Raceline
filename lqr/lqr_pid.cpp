32ee460c93ab3d26247ac350d2942bb8a3c3d659
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 20:09:39 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,33 +1,10 @@
-void LQRPID::odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr msg) {
-    geometry_msgs::msg::TransformStamped tf;
-    try {
-        // odom frame에서 map frame으로의 변환을 가져옴
-        tf = tf_buffer_->lookupTransform(global_refFrame, msg->header.frame_id, tf2::TimePointZero);
-    } catch (tf2::TransformException &ex) {
-        RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s: %s",
-                    msg->header.frame_id.c_str(), global_refFrame.c_str(), ex.what());
-        return;
-    }
-
-    // position 변환
-    tf2::Vector3 original_pos(msg->pose.pose.position.x,
-                              msg->pose.pose.position.y,
-                              msg->pose.pose.position.z);
-    tf2::Transform tf2_transform;
-    tf2::fromMsg(tf.transform, tf2_transform);
-    tf2::Vector3 transformed_pos = tf2_transform * original_pos;
-    double x = transformed_pos.x();
-    double y = transformed_pos.y();
-
-    // orientation 변환
-    tf2::Quaternion q_orig, q_rotated;
-    tf2::fromMsg(msg->pose.pose.orientation, q_orig);
-    q_rotated = tf2_transform.getRotation() * q_orig;
-    double roll, pitch, yaw;
-    tf2::Matrix3x3(q_rotated).getRPY(roll, pitch, yaw);
-
-    double velocity = msg->twist.twist.linear.x;
-
-    // publish drive message
-    publish_message(x, y, yaw, velocity);
-}
+Finished <<< f1tenth_stack [0.88s]
+--- stderr: lqr_pid                                  
+/usr/bin/ld: CMakeFiles/lqr_pid.dir/src/lqr_pid_node.cpp.o: in function `LQRPID::odom_callback(std::shared_ptr<nav_msgs::msg::Odometry_<std::allocator<void> > const>)':
+lqr_pid_node.cpp:(.text+0x5b08): undefined reference to `void tf2::fromMsg<geometry_msgs::msg::Transform_<std::allocator<void> >, tf2::Transform>(geometry_msgs::msg::Transform_<std::allocator<void> > const&, tf2::Transform&)'
+/usr/bin/ld: lqr_pid_node.cpp:(.text+0x5ba1): undefined reference to `void tf2::fromMsg<geometry_msgs::msg::Quaternion_<std::allocator<void> >, tf2::Quaternion>(geometry_msgs::msg::Quaternion_<std::allocator<void> > const&, tf2::Quaternion&)'
+collect2: error: ld returned 1 exit status
+gmake[2]: *** [CMakeFiles/lqr_pid.dir/build.make:229: lqr_pid] Error 1
+gmake[1]: *** [CMakeFiles/Makefile2:139: CMakeFiles/lqr_pid.dir/all] Error 2
+gmake: *** [Makefile:146: all] Error 2
+---
