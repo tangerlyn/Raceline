5a1adbd5301f6259c26112a2379efd621979e349
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 17:18:35 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,21 +1,24 @@
-- Rotation: in RPY (radian) [0.000, -0.000, 0.000]
-- Rotation: in RPY (degree) [0.000, -0.000, 0.000]
-- Matrix:
-  1.000  0.000  0.000  0.000
-  0.000  1.000  0.000  0.000
-  0.000  0.000  1.000  0.000
-  0.000  0.000  0.000  1.000
-At time 0.0
-- Translation: [0.000, 0.000, 0.000]
-- Rotation: in Quaternion [0.000, 0.000, 0.000, 1.000]
-- Rotation: in RPY (radian) [0.000, -0.000, 0.000]
-- Rotation: in RPY (degree) [0.000, -0.000, 0.000]
-- Matrix:
-  1.000  0.000  0.000  0.000
-  0.000  1.000  0.000  0.000
-  0.000  0.000  1.000  0.000
-  0.000  0.000  0.000  1.000
-At time 0.0
-- Translation: [0.000, 0.000, 0.000]
-- Rotation: in Quaternion [0.000, 0.000, 0.000, 1.000]
-- Rotation: in RPY (radian) [0.000,
+void PurePursuit::transformandinterp_waypoint() {  // pass old waypoint here
+    // initialise vectors
+    waypoints.lookahead_point_world << waypoints.X[waypoints.index], waypoints.Y[waypoints.index], 0.0;
+    waypoints.current_point_world << waypoints.X[waypoints.velocity_index], waypoints.Y[waypoints.velocity_index], 0.0;
+
+    visualize_lookahead_point(waypoints.lookahead_point_world);
+    visualize_current_point(waypoints.current_point_world);
+
+    // look up transformation at that instant from tf_buffer_
+    geometry_msgs::msg::TransformStamped transformStamped;
+
+    try {
+        // Get the transform from the base_link reference to world reference frame
+        transformStamped = tf_buffer_->lookupTransform(car_refFrame, global_refFrame, tf2::TimePointZero);
+    } catch (tf2::TransformException &ex) {
+        RCLCPP_INFO(this->get_logger(), "Could not transform. Error: %s", ex.what());
+    }
+
+    // transform points (rotate first and then translate)
+    Eigen::Vector3d translation_v(transformStamped.transform.translation.x, transformStamped.transform.translation.y, transformStamped.transform.translation.z);
+    quat_to_rot(transformStamped.transform.rotation.w, transformStamped.transform.rotation.x, transformStamped.transform.rotation.y, transformStamped.transform.rotation.z);
+
+    waypoints.lookahead_point_car = (rotation_m * waypoints.lookahead_point_world) + translation_v;
+}
