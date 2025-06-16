2d1ddd7a845abb555de3d350f18d7071580ab437
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 20:19:59 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,6 +1,3 @@
-RCLCPP_INFO(this->get_logger(), "LQR idx=%d, e=%.3f, th_e=%.3f, psi=%.3f, yaw=%.3f",
-            ind, e, th_e, waypoints.psi_rad[ind], yaw);
-
-
-yaw += M_PI_2;
-yaw = pi2pi(yaw);
+/home/misys/f1tenth_ws/src/lqr_pid/src/lqr_pid_node.cpp:249:1: error: expected unqualified-id before ‘do’
+  249 | RCLCPP_INFO(this->get_logger(), "LQR idx=%d, e=%.3f, th_e=%.3f, psi=%.3f, yaw=%.3f",
+      | ^~~~~~~~~~~
