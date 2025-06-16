b38b5687b9654f63d7435e3ebc285860f27d0ad5
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 20:40:48 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,100 +1,15 @@
-misys@AIX-411-UBUNTU-22:~/f1tenth_ws/src/lqr_pid$ ros2 topic echo /tf_static 
-transforms:
-- header:
-    stamp:
-      sec: 1750073332
-      nanosec: 46160514
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/back_left_wheel
-  transform:
-    translation:
-      x: 0.0
-      y: 0.12065
-      z: 0.0508
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
-- header:
-    stamp:
-      sec: 1750073332
-      nanosec: 46160514
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/back_right_wheel
-  transform:
-    translation:
-      x: 0.0
-      y: -0.12065
-      z: 0.0508
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
-- header:
-    stamp:
-      sec: 1750073332
-      nanosec: 46160514
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/front_left_hinge
-  transform:
-    translation:
-      x: 0.3302
-      y: 0.12065
-      z: 0.0508
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
-- header:
-    stamp:
-      sec: 1750073332
-      nanosec: 46160514
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/front_right_hinge
-  transform:
-    translation:
-      x: 0.3302
-      y: -0.12065
-      z: 0.0508
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
-- header:
-    stamp:
-      sec: 1750073332
-      nanosec: 46160514
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/laser_model
-  transform:
-    translation:
-      x: 0.275
-      y: 0.0
-      z: 0.165
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750073693
-      nanosec: 733544183
-    frame_id: map
-  child_frame_id: odom
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
+96, yaw=-1.739
+[lqr_pid-1] [INFO] [1750074034.713668616] [lqr_pid_node]: LQR idx=113, e=0.496, th_e=-0.608, psi=-0.996, yaw=-1.604
+[lqr_pid-1] [INFO] [1750074034.741309389] [lqr_pid_node]: LQR idx=113, e=0.483, th_e=-0.457, psi=-0.996, yaw=-1.453
+[lqr_pid-1] [INFO] [1750074034.763715487] [lqr_pid_node]: LQR idx=114, e=-0.440, th_e=-0.479, psi=-0.921, yaw=-1.400
+[lqr_pid-1] [INFO] [1750074034.786599717] [lqr_pid_node]: LQR idx=113, e=0.403, th_e=-0.409, psi=-0.996, yaw=-1.405
+[lqr_pid-1] [INFO] [1750074034.815301215] [lqr_pid_node]: LQR idx=113, e=0.332, th_e=-0.204, psi=-0.996, yaw=-1.201
+[lqr_pid-1] [INFO] [1750074034.839273528] [lqr_pid_node]: LQR idx=113, e=0.279, th_e=-0.131, psi=-0.996, yaw=-1.127
+[lqr_pid-1] [INFO] [1750074034.861970225] [lqr_pid_node]: LQR idx=113, e=0.285, th_e=-0.062, psi=-0.996, yaw=-1.058
+[lqr_pid-1] [INFO] [1750074034.888887094] [lqr_pid_node]: LQR idx=113, e=0.234, th_e=-0.025, psi=-0.996, yaw=-1.022
+[lqr_pid-1] [INFO] [1750074034.912655240] [lqr_pid_node]: LQR idx=113, e=0.179, th_e=0.032, psi=-0.996, yaw=-0.965
+[lqr_pid-1] [INFO] [1750074034.937705074] [lqr_pid_node]: LQR idx=113, e=0.123, th_e=0.173, psi=-0.996, yaw=-0.824
+[lqr_pid-1] [INFO] [1750074034.977332556] [lqr_pid_node]: LQR idx=113, e=0.116, th_e=0.142, psi=-0.996, yaw=-0.854
+[lqr_pid-1] [INFO] [1750074034.990043264] [lqr_pid_node]: LQR idx=113, e=0.192, th_e=-0.035, psi=-0.996, yaw=-1.031
+[lqr_pid-1] [INFO] [1750074035.013512960] [lqr_pid_node]: LQR idx=113, e=-0.124, th_e=0.250, psi=-0.996, yaw=-0.746
+[lqr_pid-1] [INFO] [1750074035.041775352] [lqr_pid_node]: LQR idx=113, e=-0.143, th_e=0.130, psi=-0.996, yaw=-0.866
