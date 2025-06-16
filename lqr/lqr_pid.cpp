f33ae202f35434cfbfb1f7d8a9e32d158ed2dc50
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 16:49:17 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,55 +1,77 @@
-scripts' will not be supported in future versions. Please use the underscore name 'install_scripts' instead
-  warnings.warn(
----
-Finished <<< safety_node [2.34s]
-Finished <<< realsense2_description [0.65s]                                  
-Finished <<< vesc_ackermann [0.70s]
-Finished <<< realsense2_camera [0.73s]                                       
-Finished <<< slam_toolbox [2.51s]
-Finished <<< serial_driver [0.28s]                                          
-Starting >>> vesc_driver
-Finished <<< udp_driver [0.32s]
-Finished <<< key_teleop [2.67s]
---- stderr: f1tenth_gym_ros
-/usr/lib/python3/dist-packages/setuptools/dist.py:723: UserWarning: Usage of dash-separated 'script-dir' will not be supported in future versions. Please use the underscore name 'script_dir' instead
-  warnings.warn(
----
-Finished <<< f1tenth_gym_ros [2.67s]
---- stderr: particle_filter
-/usr/lib/python3/dist-packages/setuptools/dist.py:723: UserWarning: Usage of dash-separated 'script-dir' will not be supported in future versions. Please use the underscore name 'script_dir' instead
-  warnings.warn(
----
-Finished <<< particle_filter [2.66s]
-Finished <<< mouse_teleop [2.70s]                                                  
-Finished <<< vesc_driver [0.16s]
-Starting >>> vesc
-Finished <<< bringup [2.78s]
-Finished <<< vesc [0.08s]                                                                       
-Finished <<< joy_teleop [1.41s]                                                   
-Starting >>> teleop_tools
-Starting >>> f1tenth_stack
-/usr/lib/python3/dist-packages/setuptools/dist.py:723: UserWarning: Usage of dash-separated 'script-dir' will not be supported in future versions. Please use the underscore name 'script_dir' instead
-  warnings.warn(
-/usr/lib/python3/dist-packages/setuptools/dist.py:723: UserWarning: Usage of dash-separated 'install-scripts' will not be supported in future versions. Please use the underscore name 'install_scripts' instead
-  warnings.warn(
-Finished <<< teleop_tools [0.32s]                                                               
---- stderr: f1tenth_stack                                                            
-/usr/lib/python3/dist-packages/setuptools/dist.py:723: UserWarning: Usage of dash-separated 'script-dir' will not be supported in future versions. Please use the underscore name 'script_dir' instead
-  warnings.warn(
----
-Finished <<< f1tenth_stack [0.90s]
---- stderr: lqr_pid                                  
-/usr/bin/ld: CMakeFiles/lqr_pid.dir/src/lqr_pid_node.cpp.o: in function `geometry_msgs::msg::PoseStamped_<std::allocator<void> >& tf2_ros::BufferInterface::transform<geometry_msgs::msg::PoseStamped_<std::allocator<void> > >(geometry_msgs::msg::PoseStamped_<std::allocator<void> > const&, geometry_msgs::msg::PoseStamped_<std::allocator<void> >&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&, std::chrono::duration<long, std::ratio<1l, 1000000000l> >) const':
-lqr_pid_node.cpp:(.text._ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE[_ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE]+0x5a): undefined reference to `std::chrono::time_point<std::chrono::_V2::system_clock, std::chrono::duration<long, std::ratio<1l, 1000000000l> > > tf2::getTimestamp<geometry_msgs::msg::PoseStamped_<std::allocator<void> > >(geometry_msgs::msg::PoseStamped_<std::allocator<void> > const&)'
-/usr/bin/ld: lqr_pid_node.cpp:(.text._ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE[_ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE]+0x7a): undefined reference to `std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > tf2::getFrameId<geometry_msgs::msg::PoseStamped_<std::allocator<void> > >(geometry_msgs::msg::PoseStamped_<std::allocator<void> > const&)'
-/usr/bin/ld: lqr_pid_node.cpp:(.text._ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE[_ZNK7tf2_ros15BufferInterface9transformIN13geometry_msgs3msg12PoseStamped_ISaIvEEEEERT_RKS7_S8_RKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENSt6chrono8durationIlSt5ratioILl1ELl1000000000EEEE]+0xcf): undefined reference to `void tf2::doTransform<geometry_msgs::msg::PoseStamped_<std::allocator<void> > >(geometry_msgs::msg::PoseStamped_<std::allocator<void> > const&, geometry_msgs::msg::PoseStamped_<std::allocator<void> >&, geometry_msgs::msg::TransformStamped_<std::allocator<void> > const&)'
-collect2: error: ld returned 1 exit status
-gmake[2]: *** [CMakeFiles/lqr_pid.dir/build.make:230: lqr_pid] Error 1
-gmake[1]: *** [CMakeFiles/Makefile2:139: CMakeFiles/lqr_pid.dir/all] Error 2
-gmake: *** [Makefile:146: all] Error 2
----
-Failed   <<< lqr_pid [11.0s, exited with code 2]
+misys@AIX-411-UBUNTU-22:~/f1tenth_ws$ cat src/lqr_pid/CMakeLists.txt 
+cmake_minimum_required(VERSION 3.8)
+project(lqr_pid)
 
-Summary: 27 packages finished [11.3s]
-  1 package failed: lqr_pid
-  6 packages had stderr output: f1tenth_gym_ros f1tenth_stac
+# Default to C++14
+if(NOT CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 14)
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  add_compile_options(-Wall -Wextra -Wpedantic)
+endif()
+
+# find dependencies
+foreach(PACKAGE
+  ament_cmake
+  rclcpp
+  rclpy
+  geometry_msgs
+  nav_msgs
+  ackermann_msgs
+  sensor_msgs
+  std_msgs
+  visualization_msgs
+  eigen3_cmake_module
+  Eigen3
+  tf2_ros
+  tf2
+  tf2_geometry_msgs
+  )
+
+  find_package(${PACKAGE} REQUIRED)
+
+endforeach()
+
+# Include Cpp "include" directory
+include_directories(include)
+include_directories(
+  ${EIGEN3_INCLUDE_DIRS}
+)
+
+# Create Cpp executable
+add_executable(lqr_pid src/lqr_pid_node.cpp)
+
+ament_target_dependencies(lqr_pid
+  rclcpp geometry_msgs ackermann_msgs nav_msgs sensor_msgs std_msgs visualization_msgs tf2_ros tf2 eigen3_cmake_module Eigen3 tf2_geometry_msgs
+)
+
+add_executable(waypoint_visualizer src/waypoint_visualizer.cpp)
+
+ament_target_dependencies(waypoint_visualizer
+  rclcpp geometry_msgs std_msgs visualization_msgs eigen3_cmake_module Eigen3
+)
+
+# Install Cpp executables
+install(TARGETS
+  lqr_pid
+  waypoint_visualizer
+  DESTINATION lib/${PROJECT_NAME})
+
+# Install launch files and config
+install(DIRECTORY launch DESTINATION share/${PROJECT_NAME})
+install(DIRECTORY config DESTINATION share/${PROJECT_NAME})
+
+if(BUILD_TESTING)
+  find_package(ament_lint_auto REQUIRED)
+  # the following line skips the linter which checks for copyrights
+  # comment the line when a copyright and license is added to all source files
+  set(ament_cmake_copyright_FOUND TRUE)
+  # the following line skips cpplint (only works in a git repo)
+  # comment the line when this package is in a git repo and when
+  # a copyright and license is added to all source files
+  set(ament_cmake_cpplint_FOUND TRUE)
+  ament_lint_auto_find_test_dependencies()
+endif()
+
+ament_package()
