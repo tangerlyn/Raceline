50c8f40ed88c9899b62f88d0562bb0f308569761
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-06-16 20:03:46 +0900

    Update lqr_pid.cpp

--- lqr/lqr_pid.cpp
+++ lqr/lqr_pid.cpp
@@ -1,127 +1,33 @@
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 374422204
-    frame_id: ego_racecar/front_right_hinge
-  child_frame_id: ego_racecar/front_right_wheel
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 374422204
-    frame_id: map
-  child_frame_id: laser
-  transform:
-    translation:
-      x: -11.070623243221863
-      y: -0.02955028754656039
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: -0.0
-      z: -0.0003194320854121876
-      w: -0.9999999489815701
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 400523943
-    frame_id: sim
-  child_frame_id: ego_racecar/base_link
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 400523943
-    frame_id: ego_racecar/base_link
-  child_frame_id: ego_racecar/laser
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 400523943
-    frame_id: ego_racecar/front_left_hinge
-  child_frame_id: ego_racecar/front_left_wheel
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 400523943
-    frame_id: ego_racecar/front_right_hinge
-  child_frame_id: ego_racecar/front_right_wheel
-  transform:
-    translation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: 0.0
-      z: 0.0
-      w: 1.0
----
-transforms:
-- header:
-    stamp:
-      sec: 1750062950
-      nanosec: 400523943
-    frame_id: map
-  child_frame_id: laser
-  transform:
-    translation:
-      x: -11.035759456914924
-      y: -0.030613278861875502
-      z: 0.0
-    rotation:
-      x: 0.0
-      y: -0.0
-      z: -0.00042778460234708064
-      w: -0.9999999085001628
----
+void LQRPID::odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr msg) {
+    geometry_msgs::msg::TransformStamped tf;
+    try {
+        // odom frame에서 map frame으로의 변환을 가져옴
+        tf = tf_buffer_->lookupTransform(global_refFrame, msg->header.frame_id, tf2::TimePointZero);
+    } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s: %s",
+                    msg->header.frame_id.c_str(), global_refFrame.c_str(), ex.what());
+        return;
+    }
+
+    // position 변환
+    tf2::Vector3 original_pos(msg->pose.pose.position.x,
+                              msg->pose.pose.position.y,
+                              msg->pose.pose.position.z);
+    tf2::Transform tf2_transform;
+    tf2::fromMsg(tf.transform, tf2_transform);
+    tf2::Vector3 transformed_pos = tf2_transform * original_pos;
+    double x = transformed_pos.x();
+    double y = transformed_pos.y();
+
+    // orientation 변환
+    tf2::Quaternion q_orig, q_rotated;
+    tf2::fromMsg(msg->pose.pose.orientation, q_orig);
+    q_rotated = tf2_transform.getRotation() * q_orig;
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(q_rotated).getRPY(roll, pitch, yaw);
+
+    double velocity = msg->twist.twist.linear.x;
+
+    // publish drive message
+    publish_message(x, y, yaw, velocity);
+}
